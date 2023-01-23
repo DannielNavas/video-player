e41904b4772b3b7b49bde9e39f86740bd659b9bb
Danniel Navas <dannielnavas@gmail.com>
2023-01-23 06:23:45 -0500

    🐛 Fix player

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,21 +1,38 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
 
-MediaPlayer.prototype.play = function () {
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
+};
+
+MediaPlayer.prototype.play = function() {
   this.media.play();
 };
 
-MediaPlayer.prototype.pause = function () {
+MediaPlayer.prototype.pause = function() {
   this.media.pause();
 };
 
-MediaPlayer.prototype.togglePlay = function () {
+MediaPlayer.prototype.togglePlay = function() {
   if (this.media.paused) {
     this.play();
   } else {
     this.pause();
   }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
 };
 
 export default MediaPlayer;
