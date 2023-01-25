ab3ca2cc719a36f475662fda914221ef2c9e33da
Danniel Navas <dannielnavas@gmail.com>
2023-01-25 07:08:00 -0500

    ✨ visibilitychange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -3,6 +3,7 @@ class AutoPause {
     this.threshold = 0.25;
     //TODO: se hace referencia permanente al this
     this.handlerInterception = this.handlerInterception.bind(this);
+    this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this);
   }
   run(player) {
     //TODO: se guarda en una instacia de la clase el player
@@ -12,11 +13,22 @@ class AutoPause {
       threshold: this.threshold,
     });
     observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this.handlerVisibilityChange);
   }
 
   handlerInterception(entries) {
     const entry = entries[0];
     const isVisible = entries.intersectionRatio >= this.threshold;
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handlerVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
     if (isVisible) {
       this.player.play();
     } else {
