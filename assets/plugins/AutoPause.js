f3567773329845b7a726809d3e8c31893247dac8
Danniel Navas <dannielnavas@gmail.com>
2023-01-25 07:01:44 -0500

    ✨ IntersectionObserver

---
+++ assets/plugins/AutoPause.js
@@ -0,0 +1,28 @@
+class AutoPause {
+  constructor() {
+    this.threshold = 0.25;
+    //TODO: se hace referencia permanente al this
+    this.handlerInterception = this.handlerInterception.bind(this);
+  }
+  run(player) {
+    //TODO: se guarda en una instacia de la clase el player
+    this.player = player;
+    //TODO: el threshold es el porcentaje de visibilidad que debe tener el elemento para que se dispare el evento
+    const observer = new IntersectionObserver(this.handlerInterception, {
+      threshold: this.threshold,
+    });
+    observer.observe(this.player.media);
+  }
+
+  handlerInterception(entries) {
+    const entry = entries[0];
+    const isVisible = entries.intersectionRatio >= this.threshold;
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+}
+
+export default AutoPause;
