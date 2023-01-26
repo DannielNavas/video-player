44165cffbf16a8df5f27b73d00df8473f4b6f431
Danniel Navas <dannielnavas@gmail.com>
2023-01-26 06:36:01 -0500

    ✨ basic types

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -10,3 +10,41 @@ const sum = add(2, 3);
 // TODO: de las dos formas se puede declarar una variable booleana
 let muted: boolean = true;
 let muted2 = true;
+
+// Numeros
+//TODO: Hace analisis estatico de los tipos de datos que se le asignan a las variables
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = "Richard";
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+// people.push(9000); // Error
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("Ricardo");
+peopleAndNumbers.push(9001);
+
+// Enum
+
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+  Amarillo = "Amarillo",
+}
+
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: "Wildcard" };
+
+// Object
+let someObject: object = { type: "Wildcard" };
